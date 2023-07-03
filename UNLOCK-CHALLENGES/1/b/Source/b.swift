cc9e0b405e3168024e9230b772bf400f2f3d11fe
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-03 10:25:24 -0300

    Create b.swift

---
+++ UNLOCK-CHALLENGES/1/b/Source/b.swift
@@ -0,0 +1,8 @@
+// includes support for animations, documents, drawing and printing, text management and display, search, app extensions, resource management, and getting information about the current device.
+import UIKit
+
+let label: String = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+print(widthLabel)
